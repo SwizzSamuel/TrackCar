549015a070f888716725564b1f813a3ac559a576
Samuel <samuelgift09@gmail.com>
2024-09-12 20:37:24 +0100

    Fixed Responsiveness

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
@@ -26,33 +25,33 @@ const theme = createTheme({
 const App = () => {
     return (
         <div className="container">
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Router>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Button color="inherit" component={Link} to="/">Home</Button>
-                        <Button color="inherit" component={Link} to="/register">Register Vehicle</Button>
-                        <Button color="inherit" component={Link} to="/update-mileage">Update Mileage</Button>
-                        <Button color="inherit" component={Link} to="/update-stolen-status">Update Stolen Status</Button>
-                        <Button color="inherit" component={Link} to="/display-vehicle-information">Display Vehicle Information</Button>
-                        <Button color="inherit" component={Link} to="/retrieve-ownership-history">Retrieve Ownership History</Button>
-                        <Button color="inherit" component={Link} to="/transfer-ownership">Transfer Ownership</Button>
-                    </Toolbar>
-                </AppBar>
-                <Container>
-                    <Switch>
-                        <Route path="/" exact component={HomePage} />
-                        <Route path="/register" component={VehicleForm} />
-                        <Route path="/update-mileage" component={UpdateMileage} />
-                        <Route path="/update-stolen-status" component={UpdateStolenStatus} />
-                        <Route path="/display-vehicle-information" component={DisplayVehicleInformation} />
-                        <Route path="/retrieve-ownership-history" component={RetrieveOwnershipHistory} />
-                        <Route path="/transfer-ownership" component={TransferOwnership} />
-                    </Switch>
-                </Container>
-            </Router>
-        </ThemeProvider>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Router>
+                    <AppBar position="static">
+                        <Toolbar className="toolbar">
+                            <Button color="inherit" component={Link} to="/">Home</Button>
+                            <Button color="inherit" component={Link} to="/register">Register Vehicle</Button>
+                            <Button color="inherit" component={Link} to="/update-mileage">Update Mileage</Button>
+                            <Button color="inherit" component={Link} to="/update-stolen-status">Update Stolen Status</Button>
+                            <Button color="inherit" component={Link} to="/display-vehicle-information">Display Vehicle Information</Button>
+                            <Button color="inherit" component={Link} to="/retrieve-ownership-history">Retrieve Ownership History</Button>
+                            <Button color="inherit" component={Link} to="/transfer-ownership">Transfer Ownership</Button>
+                        </Toolbar>
+                    </AppBar>
+                    <Container>
+                        <Switch>
+                            <Route path="/" exact component={HomePage} />
+                            <Route path="/register" component={VehicleForm} />
+                            <Route path="/update-mileage" component={UpdateMileage} />
+                            <Route path="/update-stolen-status" component={UpdateStolenStatus} />
+                            <Route path="/display-vehicle-information" component={DisplayVehicleInformation} />
+                            <Route path="/retrieve-ownership-history" component={RetrieveOwnershipHistory} />
+                            <Route path="/transfer-ownership" component={TransferOwnership} />
+                        </Switch>
+                    </Container>
+                </Router>
+            </ThemeProvider>
         </div>
     );
 };
